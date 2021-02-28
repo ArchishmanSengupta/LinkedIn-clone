d2bba5da92a47dcb17c17ca64930dae213938ef7
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-02-28 19:08:45 +0530

    navbar complete

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@material-ui/icons/Search'
 import HeaderOption from './HeaderOption'
 import HomeIcon from '@material-ui/icons/Home'
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
 function Header() {
     return (
         <div className='header'>
@@ -18,9 +21,12 @@ function Header() {
 
             </div>
             <div className="header__right">
-                <HeaderOption Icon={HomeIcon} title ='Home'/>
-                <HeaderOption Icon={SupervisorAccountIcon}title='My Network'/>
-
+                <HeaderOption Icon={HomeIcon} title ="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title ="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title ="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title ="Notifications"/>
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4E03AQENP7BiSXgosQ/profile-displayphoto-shrink_800_800/0/1611595766714?e=1620259200&v=beta&t=IesVmaQSErzHTwz7ODXOMxGuVPcqYS838eoeNJ3Wr_U" title='me'/>
 
             </div>
         </div>
